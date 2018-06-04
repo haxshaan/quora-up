06593c1735e833758c0b0119ba89e656b4b26a36
Shantam Mathuria <shantam.m22@gmail.com>
2018-06-04 10:49:39 +0530

    Codes written in Pycharm
    Initial commit

---
+++ main.py
@@ -0,0 +1,25 @@
+__author__ = "Shantam Mathuria"
+__copyright__ = "Copyright 2018, The Cogent Project"
+__credits__ = ["Shantam Mathuria"]
+__license__ = "GPL"
+__version__ = "1.0.1"
+__maintainer__ = "Shantam Mathuria"
+__email__ = "shantam.m22@gmail.com"
+__status__ = "Production"
+
+import time
+from configparser import ConfigParser
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+
+
+# Reading the configuration file
+par = ConfigParser()
+par.read('config.ini')
+parameters = {}
+
+# Dictionary for storing the parsed parameters
+for section_name in par.sections():
+        # Parsing the configuration file and reading it into the dict
+        for name, value in par.items(section_name):
+                parameters[name] = value
